e5db1c66db7845f8ef1a6de1a54e13399d7cc447
sabaakram12 <168063496+sabaakram12@users.noreply.github.com>
2024-07-24 12:56:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let boxes = document.querySelectorAll(".box");
+let  resetBtn =document.querySelector("#reset-btn");
+
+
+let turnO = true; //playerx , playeryO;
+
+const winPatterns = [
+    [0,1,2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener('click', () =>{
+        console.log("box was clicked");
+        if (turnO) { //playerO
+            box.innerText = 'O';
+            turnO = false;
+
+       } else {  //playerX
+       box.innerText = 'X';
+       turnO = true;
+}
+    });
+   
+
+});
